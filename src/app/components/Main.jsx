53617ff06f42071b8c56a4b47b74adb69484adcf
enemuo <francenemuo@gmail.com>
2024-07-10 15:37:12 +0100

    feat: add form ui

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -4,16 +4,16 @@ import React from 'react'
 
 export default function Main() {
   return (
-    <main className='flex md:flex-row flex-col'>
-        <div>
-            <h1 className='capitalize text-6xl font-semibold'>Manage your tasks Quickly</h1>
-            <p>Effectively manage your tasks daily, weekly or even yearly, not missing out on anything.</p>
+    <main className='grid md:grid-cols-2 gap-8'>
+        <div className='flex flex-col gap-y-10 items-start'>
+            <h1 className='capitalize text-5xl font-semibold'>Manage your tasks Quickly</h1>
+            <p className='text-3xl'>Effectively manage your tasks daily, weekly or even yearly, not missing out on anything.</p>
             <button>
-                <Link href={'/getstarted'} className='bg-green-900 py-2 px-4 rounded-md shadow-lg text-center text-white'>Get started</Link>
+                <Link href={'/getstarted'} className='bg-green-900 py-2 px-4 rounded-md shadow-lg text-center text-white'>Create a task</Link>
             </button>
         </div>
         <div>
-            <Image src='/task-head.jpg' alt="manage your tasks" width='500' height='500'/>
+            <Image src='/another-task.jpg' alt="manage your tasks" width='500' height='500' className='rounded-full'/>
 
         </div>
     </main>
--- src/app/getstarted/page.jsx
+++ src/app/getstarted/page.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import styles from './get.module.css'
 
 export default function GetStarted() {
   return (
-    <div>Get Started now</div>
+    <div className={styles.form}>
+      <form>
+        <fieldset>
+        <legend>create a new task</legend>
+        <ul>
+          <li>
+            <input type="text" name="title" id="title" placeholder='task title' defaultValue={''} required/>
+          </li>
+          <li>
+            <input type="text" name="desc" id="desc" placeholder='task description' defaultValue={''} required/>
+          </li>
+          <li>
+            <input type="date" name="date" id="date" required />
+          </li>
+          <li>
+            <button className='uppercase'>create a task</button>
+          </li>
+        </ul>
+        </fieldset>
+      </form>
+    </div>
   )
 }
