4724f16e351e53a1f53ca9ea802a84053a2b56d4
enemuo <francenemuo@gmail.com>
2024-07-10 19:43:08 +0100

    feat: handle some components creation

---
+++ src/app/error.jsx
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+
+export default function Error() {
+  return (
+    <div>
+        <h1>Sorry, you are required to have some details here</h1>
+    </div>
+  )
+}
